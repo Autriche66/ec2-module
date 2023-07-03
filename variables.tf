8aaa914582d637b07e3248f5c0aafb6ee8146f74
Anne <tchokouafe2005@yahoo.fr>
2023-07-03 16:26:33 -0700

    terraform

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+    description = "amazon machine image"
+    type = string
+    default = "ami-04823729c75214919"
+
+}
+variable "instanceType" {
+    default = "t2.small"
+}
+  
+variable "region_name" {
+    
+}
+variable "profile_name"  {
+    default = "default"
+}
+
+  
+ 
